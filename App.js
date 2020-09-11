5f9bb5c497807b387cf75d1b5e4b8a0fbab3dd29
hrupesh <rcashok0@gmail.com>
2020-09-11 20:25:32 +0530

    Auto Commit 😁

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import App from "./src/App";
 
-export default function App() {
+export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <App />
+      <StatusBar backgroundColor="#000D" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View, StatusBar} from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.view}>
+      <Text>Welcome Again!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    view:{
+        padding:10,
+        backgroundColor:"#000D",
+        height:"100%",
+        color:"#fff"
+    }
+});
